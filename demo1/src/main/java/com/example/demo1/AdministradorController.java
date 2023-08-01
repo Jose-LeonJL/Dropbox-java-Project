0a626c8cc0831fba9829c41906cf7d2d8a957f0f
Jose_LeonJL <60480200+Jose-LeonJL@users.noreply.github.com>
2023-08-01 12:57:29 -0600

    avance

--- demo1/src/main/java/com/example/demo1/AdministradorController.java
+++ demo1/src/main/java/com/example/demo1/AdministradorController.java
@@ -1,14 +1,18 @@
 package com.example.demo1;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class AdministradorController {
-    @FXML
-    protected void OnLoginButtonClick() throws IOException {
-        AdministradorApp administradorApp = new AdministradorApp();
-        administradorApp.start(new Stage());
+public class AdministradorController implements Initializable {
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
     }
 }
--- demo1/src/main/java/com/example/demo1/LoginController.java
+++ demo1/src/main/java/com/example/demo1/LoginController.java
@@ -2,27 +2,78 @@ package com.example.demo1;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class LoginController {
+public class LoginController implements Initializable {
     @FXML
-    private Button myButton;
+    private Label lblErrors;
 
     @FXML
-    protected void onHelloButtonClick() {
+    private TextField txtUsername;
+
+    @FXML
+    private TextField txtPassword;
+
+    @FXML
+    private Button btnSignin;
+
+    @FXML
+    public void OnLoginButtonClick(MouseEvent event) {
+        try {
+
+            //add you loading or delays - ;-)
+            Node node = (Node) event.getSource();
+            Stage stage = (Stage) node.getScene().getWindow();
+            //stage.setMaximized(true);
+            stage.close();
+            Scene scene = new Scene(FXMLLoader.load(getClass().getResource("Login-View.fxml")));
+            stage.setScene(scene);
+            stage.show();
+
+        } catch (IOException ex) {
+            System.err.println(ex.getMessage());
+        }
     }
 
-    public void OnLoginButtonClick(ActionEvent event) throws IOException {
-        Stage currentStage = (Stage) ((javafx.scene.Node) event.getSource()).getScene().getWindow();
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
 
-        AdministradorApp administradorApp = new AdministradorApp();
-        administradorApp.start(new Stage());
-        currentStage.close();
+    }
+
+    @FXML
+    public void handleButtonAction(MouseEvent event) {
+
+            //login here
+                try {
+
+                    //add you loading or delays - ;-)
+                    Node node = (Node) event.getSource();
+                    Stage stage = (Stage) node.getScene().getWindow();
+                    //stage.setMaximized(true);
+                    stage.close();
+                    Scene scene = new Scene(FXMLLoader.load(getClass().getResource("Administrador-View.fxml")));
+                    stage.setScene(scene);
+                    stage.show();
+
+                } catch (IOException ex) {
+
+                    System.err.println(ex.getMessage());
+                }catch (Exception ex){
+                    System.out.println(ex.getMessage());
+                }
 
     }
 }
